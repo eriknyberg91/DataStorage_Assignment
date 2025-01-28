d1756848c495cadecfb055e79492b12933a6cc91
Erik <erikmikaelnyberg@gmail.com>
2025-01-28 10:58:07 +0100

    Add project files.

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\DatabaseProjects\\DataStorage_Assignment\\Data\\Databases\\datastorage_assignment_local_db.mdf;Integrated Security=True;Connect Timeout=30");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
