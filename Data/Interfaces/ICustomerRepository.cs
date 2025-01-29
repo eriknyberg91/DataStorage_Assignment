07397cdce31beba905f1dc68d55b2ad977d9b66f
Erik <erikmikaelnyberg@gmail.com>
2025-01-29 10:37:38 +0100

    Added interface and repository for CustomerEntity

---
+++ Data/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Data.Entities;
+using System.Linq.Expressions;
+
+namespace Data.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task<CustomerEntity> CreateAsync(CustomerEntity entity);
+    Task<IEnumerable<CustomerEntity>> GetAllAsync();
+    Task<CustomerEntity> GetAsync(int id);
+    Task<CustomerEntity> UpdateAsync(CustomerEntity updatedEntity);
+    Task<bool> DeleteAsync(int id);
+}
--- Data/Repositories/CustomerRepository.cs
+++ Data/Repositories/CustomerRepository.cs
@@ -1,17 +1,81 @@
-﻿using Data.Contexts;
+﻿using System.Diagnostics;
+using Data.Contexts;
+using Data.Entities;
+using Data.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories;
 
-public class CustomerRepository(DataContext context)
+public class CustomerRepository(DataContext context) : ICustomerRepository
 {
     private readonly DataContext _context = context;
 
     //CREATE
+    public async Task<CustomerEntity> CreateAsync (CustomerEntity newCustomer)
+    {
+        if (newCustomer == null)
+        {
+            return null!;
+        }
 
+        try
+        {
+            await _context.AddAsync(newCustomer);
+            await _context.SaveChangesAsync();
+            return newCustomer;
+        }
+
+        catch (Exception exception) 
+        {
+            Debug.WriteLine($"Encountered error creating new customer: {exception.Message}");
+            return null!;
+        }
+    }
+    
     //READ
+    public async Task <IEnumerable<CustomerEntity>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+
+    public async Task<CustomerEntity> GetAsync (int id)
+    {
+        var customer = await _context.Customers.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (customer == null)
+        {
+            return null!;
+        }
+
+        return customer;
+    }
 
     //UPDATE
+    public async Task<CustomerEntity> UpdateAsync (CustomerEntity updatedEntity)
+    {
+        if (updatedEntity == null)
+        {
+            return null!;
+        }
+
+        try
+        {
+            var existingEntity = await _context.Customers.FirstOrDefaultAsync(x => x.Id == updatedEntity.Id);
+            if (existingEntity == null)
+            {
+                return null!;
+            }
+
+            _context.Entry(existingEntity).CurrentValues.SetValues(updatedEntity);
+            await _context.SaveChangesAsync();
+            return updatedEntity;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error updating product entity: {ex.Message}");
+            return null!;
+        }
+    }
 
     //DELETE
     public async Task<bool> DeleteAsync(int id)
