6a86ee4b14ab553db9e80e37e46dd794ed0ec76c
Erik <erikmikaelnyberg@gmail.com>
2025-02-04 10:02:26 +0100

    CRUD functionality for Customer completed.

--- Presentation.WebApi/Controllers/CustomerController.cs
+++ Presentation.WebApi/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Dtos;
 using Business.Interfaces;
+using Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.WebApi.Controllers
@@ -10,12 +11,13 @@ namespace Presentation.WebApi.Controllers
     {
         private readonly ICustomerService _customerService = customerService;
 
+        
+        
         [HttpPost]
         public async Task<IActionResult> Create(CustomerRegistrationForm form)
         {
             if (ModelState.IsValid)
             {
-
                 if (await _customerService.CheckIfCustomerExistsAsync(x => x.CustomerName == form.CustomerName))
                 {
                     return Conflict("Customer with same name already exists.");
@@ -27,8 +29,34 @@ namespace Presentation.WebApi.Controllers
                     return Ok(product);
                 }
             }
-
             return BadRequest();
+        }
+
+        [HttpGet]
+        public async Task <IEnumerable<CustomerEntity>> GetAll ()
+        {
+            return await _customerService.GetAllCustomersAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            var customer = await _customerService.GetCustomerAsync(x => x.Id == id);
+            if (customer != null)
+            {
+                return Ok(customer);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            if (await _customerService.DeleteCustomerAsync(id))
+            {
+                return Ok();
+            }
+            return NotFound();
         }
     }
 }
